917a37d7b8b85b44d49cb471a5290a7ee3582f2d
dbeauvai <diane.beauvais@canada.ca>
2016-03-01 12:10:30 -0500

    First commit

---
+++ cvpToolApi/Models/IDrugProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cvpWebApi.Models
+{
+    interface IDrugProductRepository
+    {
+        IEnumerable<DrugProduct> GetAll();
+        DrugProduct Get(int id);
+        IEnumerable<DrugProduct> Get(string drugName);
+        // DrugProduct Add(DrugProduct drugProduct);
+        // void Remove(int id);
+        // bool Update(DrugProduct drugProduct);
+    }
+}
